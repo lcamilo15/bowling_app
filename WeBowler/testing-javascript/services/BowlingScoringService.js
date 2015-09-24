4dda5be3f51829d670746e97851e938431448d5c
FMastro <FMastro@LMGlobal.net>
2015-09-24 17:31:30 -0400

    Score JS Testing

--- WeBowler/testing-javascript/services/BowlingScoringService.js
+++ WeBowler/testing-javascript/services/BowlingScoringService.js
@@ -2,22 +2,44 @@
 
 function BowlingScoringService() {
 
-    var _frames = [];
-    var _totalScore = 0;
+    var totalScore = 0;
 
     this.getFrameCount = function() {
         return 10;
     };
 
-    this.getTotalScore = function() {
-        for (var i=0;i < _frames.length; i++){
-            var currentFrame = _frames[i];
-            _totalScore += currentFrame[0] + currentFrame[1];
+    this.getTotalScore = function (game) {
+
+        for (var i = 0; i < game.length; i++) {
+            var gameFrame = game[i];
+            var frameScore = 0;
+
+            if (gameFrame.length === 2) {
+                frameScore = (gameFrame[0] + gameFrame[1]);
+            } else {
+                frameScore = gameFrame[0];
+            }
+           
+            if (gameFrame[0] === 10) { //isStrike
+                if (i === 9) {
+                    frameScore += game[i + 1][0];
+                    i += 1; //Move ahead a frame, so we don't count the last bonus ball twice
+                } else {
+                    frameScore += game[i + 1][0] + game[i + 1][1];
+                }
+            }
+
+            if (frameScore === 10) { //isSpare
+                frameScore += game[i + 1][0];
+            }
+            totalScore += frameScore;
         }
-        return _totalScore;
+
+
+        return totalScore;
     };
 
     this.addFrameScore = function(ballOne, ballTwo){
-        _frames.push([ballOne, ballTwo]);
+       
     }
 }
