0cefbea13b294ef63abbb71844cc0a07fc55e2f8
FMastro <FMastro@LMGlobal.net>
2015-09-24 13:51:24 -0400

    Added new Test for Scoring

--- WeBowler/testing-javascript/bowlingTest.js
+++ WeBowler/testing-javascript/bowlingTest.js
@@ -5,6 +5,7 @@ describe( 'BowlingScoringServiceTest', function() {
 
     beforeEach(function() {
         bowlingScoringService = new BowlingScoringService();
+
     });
 
     afterEach(function() {
@@ -17,7 +18,15 @@ describe( 'BowlingScoringServiceTest', function() {
     });
 
     it("The total score should be equal to the sum of all the frames", function(){
-        bowlingScoringService = new BowlingScoringService();
+
+        var frames = [[5,4], [3,4], [4,8], [9,2], [4,7], [6,4]];
+        var totalScore = 0;
+        for (var i=0; i < frames.length; i++){
+            var currentFrame = frames[i];
+            bowlingScoringService.addFrameScore(currentFrame[0], currentFrame[1]);
+            totalScore += currentFrame[0] + currentFrame[1];
+        }
+        assert( totalScore === bowlingScoringService.getTotalScore(), "Total score should be " + totalScore + " but it is " + bowlingScoringService.getTotalScore());
     });
 
 });
--- WeBowler/testing-javascript/services/BowlingScoringService.js
+++ WeBowler/testing-javascript/services/BowlingScoringService.js
@@ -2,8 +2,22 @@
 
 function BowlingScoringService() {
 
+    var _frames = [];
+    var _totalScore = 0;
+
     this.getFrameCount = function() {
         return 10;
-    }
+    };
 
+    this.getTotalScore = function() {
+        for (var i=0;i < _frames.length; i++){
+            var currentFrame = _frames[i];
+            _totalScore += currentFrame[0] + currentFrame[1];
+        }
+        return _totalScore;
+    };
+
+    this.addFrameScore = function(ballOne, ballTwo){
+        _frames.push([ballOne, ballTwo]);
+    }
 }
