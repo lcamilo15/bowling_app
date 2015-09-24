5bfa8be7a86ddb9ed20ace3413258411152fc600
FMastro <FMastro@LMGlobal.net>
2015-09-24 12:25:58 -0400

    Testint a Test File. added more c# Unit Test

--- WeBowlerUnitTest/UnitTest1.cs
+++ WeBowlerUnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WeBowlerUnitTest
@@ -6,10 +7,65 @@ namespace WeBowlerUnitTest
     [TestClass]
     public class UnitTest1
     {
+        public string Message { get; set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            Debug.WriteLine("Assembly Initialize");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            Debug.WriteLine("Class Initialize");
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Message = "Hello";
+            Debug.WriteLine("Test Setup");
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("hello, world", "hello, world");
+            Debug.WriteLine("Test Method");
+            Assert.AreEqual(Message, "Hello");
+            
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Debug.WriteLine("Test Method 2");
+            Assert.AreEqual(Message, "Hello");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Debug.WriteLine("Test Cleanup");
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanUp()
+        {
+            Debug.WriteLine("Assembly CleanUp");
+        }
+
+        /*
+
+    1 - Runs [AssemblyInitialize]
+2 - Randomly runs a [ClassInitialize]
+3 - Runs the class [TestInitialize]
+4 - Randomly runs a [TestMethod] from that class
+5 - Runs the class [TestCleanup]
+Repeat 3 through 5 for each TestMethod in the class
+Repeat 2 through 5 for each test class
+6 - Runs [AssemblyCleanup]
+
+    */
     }
 }
