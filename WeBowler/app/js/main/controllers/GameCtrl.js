89eaaa96d4f25d8bd4790f31300fd3be59fa70c5
lcamilo15 <lcamilo15@gmail.com>
2015-09-25 11:13:34 -0500

    Adds bowling interface

--- WeBowler/app/js/main/controllers/GameCtrl.js
+++ WeBowler/app/js/main/controllers/GameCtrl.js
@@ -1,3 +1,17 @@
 app.controller('GameCtrl', ['$scope', function($scope) {
 
+    $scope.selectedGame = {
+        frames : [
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 },
+            { ball : [7, 3], score : 10 }
+        ]
+    };
 }]);
